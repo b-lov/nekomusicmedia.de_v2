de28906ee4bee4d3ed80e88174ed91755368fbc7
maxpiano <max@bilov.de>
2022-08-03 20:52:42 +0200

    autogenerated typesafe i18n files

---
+++ src/i18n/de/index.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef { import('../i18n-types').BaseTranslation } BaseTranslation
+ */
+
+/** @type { BaseTranslation } */
+const de = {
+	HI: 'Hallo {name:string}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n',
+	test: 'test_de'
+};
+
+export default de;
---
+++ src/i18n/en/index.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef { import('../i18n-types').Translation } Translation
+ */
+
+/** @type { Translation } */
+const en = {
+	HI: 'Hi {name}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
+	test: 'test_en'
+};
+
+export default en;
