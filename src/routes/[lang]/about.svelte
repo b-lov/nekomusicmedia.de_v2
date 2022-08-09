8145433c9791a219b476625fe1dd36c4fbf9585a
maxpiano <max@bilov.de>
2022-08-09 16:08:11 +0200

    remove hardcoded examples from about

--- src/routes/[lang]/about.svelte
+++ src/routes/[lang]/about.svelte
@@ -1,21 +1,17 @@
 <script>
 	import { LL, locale } from '$i18n/i18n-svelte';
 
-	/** @type {{name: keyof import('$i18n/i18n-types').Translation['about']['texts'], icon: String, picture: String}[]} */
-	const examples = [
-		{ name: 'experienced', icon: '', picture: '' },
-		{ name: 'personal', icon: '', picture: '' },
-		{ name: 'diverse', icon: '', picture: '' },
-		{ name: 'servicestrong', icon: '', picture: '' }
-	];
+	const examples =
+		/** @type {Array.<keyof import('$i18n/i18n-types').Translation['about']['texts']>} */
+		(Object.keys($LL.about.texts));
 </script>
 
 <h1>{$LL.about.heading()}</h1>
 <h3>{$LL.about.subheading()}</h3>
 <a href="/{$locale}/contact"><button>{$LL.contact_button()}</button></a>
 
-{#each examples as { name }}
-	<h2>{$LL.about.texts[name].title()}</h2>
-	<p>{$LL.about.texts[name].text1()}</p>
-	<p>{$LL.about.texts[name].text2()}</p>
+{#each examples as example}
+	<h2>{$LL.about.texts[example].title()}</h2>
+	<p>{$LL.about.texts[example].text1()}</p>
+	<p>{$LL.about.texts[example].text2()}</p>
 {/each}
