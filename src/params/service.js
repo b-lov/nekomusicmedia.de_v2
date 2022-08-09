9e09dd58fe74baf7770dedd5cca065961fe3bea6
maxpiano <max@bilov.de>
2022-08-09 20:15:24 +0200

    getting service slugs from LL store seems to trigger complete page reload on every [slug] visit, so meh, hardcoding it for now

--- src/params/service.js
+++ src/params/service.js
@@ -1,9 +1,4 @@
-import LL from '$i18n/i18n-svelte';
-import { get } from 'svelte/store';
-
-const services = Object.keys(get(LL).services.all);
-
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 export function match(service) {
-	return services.includes(service);
+	return ['sound', 'light', 'video', 'stage', 'presentation', 'service'].includes(service);
 }
