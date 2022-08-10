4e0a282f7a8ce6d1b26e8afdb1c1345a1b7f0e77
maxpiano <max@bilov.de>
2022-08-10 14:56:53 +0200

    add assets Fixes #7

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,20 @@
+<script>
+	/** @type {string} */
+	export let name;
+	/** @type {number} */
+	export let size = 1;
+
+	const icons = [
+		{
+			name: 'flag_EN',
+			viewBox: '0 0 512 512',
+			svg: `<path fill="#012169" d="M0 0h512v512H0z"/><path fill="#FFF" d="M512 0v64L322 256l190 187v69h-67L254 324 68 512H0v-68l186-187L0 74V0h62l192 188L440 0z"/><path fill="#C8102E" d="m184 324 11 34L42 512H0v-3l184-185zm124-12 54 8 150 147v45L308 312zM512 0 320 196l-4-44L466 0h46zM0 1l193 189-59-8L0 49V1z"/><path fill="#FFF" d="M176 0v512h160V0H176zM0 176v160h512V176H0z"/><path fill="#C8102E" d="M0 208v96h512v-96H0zM208 0v512h96V0h-96z"/>`
+		}
+	];
+
+	const displayIcon = icons.find((e) => e.name === name) || icons[0];
+</script>
+
+<svg class={$$props.class} height="{size}rem" viewBox={displayIcon.viewBox}>
+	{@html displayIcon.svg}
+</svg>
