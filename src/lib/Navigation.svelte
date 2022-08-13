47287f4d75f5be7e0a3354a5296bcd51416a0e45
maxpiano <max@bilov.de>
2022-08-14 00:18:20 +0200

    add Navigation

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,8 +1,38 @@
-<script>
-	import LL from '$i18n/i18n-svelte.js';
-	import { locale } from '$i18n/i18n-svelte';
+<script context="module">
+	import { writable } from 'svelte/store';
+	import { navigating } from '$app/stores';
+
+	export const navMenuOpen = writable(false);
+	export const toggleMenu = () => {
+		navMenuOpen.update((n) => (n = !n));
+	};
 </script>
 
-<a href="/{$locale}/about">{$LL.about.title()}</a>
-<a href="/{$locale}/services">{$LL.services.title()}</a>
-<a href="/{$locale}/contact">{$LL.contact.title()}</a>
+<script>
+	import { LL, locale } from '$i18n/i18n-svelte';
+	import { slide } from 'svelte/transition';
+
+	/** @type { Array.<keyof Pick<import('$i18n/i18n-types').Translation, 'about' | 'services' | 'contact'>> } */
+	const links = ['about', 'services', 'contact'];
+
+	$: if ($navigating) {
+		navMenuOpen.set(false);
+	}
+</script>
+
+{#if $navMenuOpen}
+	<nav transition:slide>
+		{#each links as link}
+			<a href="/{$locale}/{link}">{$LL[link].title()}</a>
+		{/each}
+	</nav>
+{/if}
+
+<style lang="postcss">
+	nav {
+		@apply flex flex-col;
+	}
+	a {
+		@apply p-4 font-oswald text-lg uppercase w-full text-right pr-6;
+	}
+</style>
