4e5aa5fc6e8ae0320b845aa0606624aab2e126ea
maxpiano <max@bilov.de>
2022-08-13 14:42:27 +0200

    add nav button

---
+++ src/lib/NavButton.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { toggleMenu } from '$lib/Navigation.svelte';
+</script>
+
+<button on:click={toggleMenu}>Nav</button>
+
+<style lang="postcss">
+	button {
+		@apply bg-red-100 aspect-square w-14;
+	}
+</style>
