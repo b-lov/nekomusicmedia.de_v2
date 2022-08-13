cc8bcd0b2d2098e03da1fb845c2f5042a920d8ac
maxpiano <max@bilov.de>
2022-08-13 12:26:08 +0200

    add plex and oswald fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
+				oswald: ['Oswald']
+			}
+		}
 	},
-
 	plugins: [require('@tailwindcss/typography')]
 };
 
