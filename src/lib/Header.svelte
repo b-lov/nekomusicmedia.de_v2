15519a14da164ece701314f0cdfff91834dd0a49
maxpiano <max@bilov.de>
2022-08-14 00:19:11 +0200

    make header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,26 @@
 <script>
+	import { locale } from '$i18n/i18n-svelte';
+	import NavButton from './Hamburger.svelte';
 	import Icon from './Icon.svelte';
 	import Navigation from './Navigation.svelte';
+	import Headroom from './Headroom.svelte';
 </script>
 
-<a href="/">
-	<Icon size={4} />
-</a>
-<Navigation />
+<Headroom>
+	<header>
+		<div>
+			<a href="/{$locale}"><Icon size={3.2} /></a>
+			<NavButton />
+		</div>
+		<Navigation />
+	</header>
+</Headroom>
+
+<style lang="postcss">
+	header {
+		@apply border-b-2 w-full bg-white;
+	}
+	div {
+		@apply flex justify-between items-center pl-3 overflow-hidden;
+	}
+</style>
