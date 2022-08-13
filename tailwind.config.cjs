b14ce4c32b72d94e818e95378f901b50ea98cd3a
maxpiano <max@bilov.de>
2022-08-13 14:28:53 +0200

    remove aspect ratio tailwind plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,11 +10,7 @@ const config = {
 			}
 		}
 	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/aspect-ratio')
-	]
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
 };
 
 module.exports = config;
