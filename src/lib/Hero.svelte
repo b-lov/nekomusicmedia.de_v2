718cda6aa5035ed278fe60c98ab1eda2fc6855ca
maxpiano <max@bilov.de>
2022-08-14 17:01:58 +0200

    enable css nesting, nest hero component

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -14,31 +14,27 @@
 	<div>
 		<h1>{heading}</h1>
 		<h3>{subheading}</h3>
-		<a href="/{$locale}/contact">
-			<Button>
-				{$LL.contact_button()}
-			</Button>
-		</a>
+		<a href="/{$locale}/contact"><Button>{$LL.contact_button()}</Button></a>
 	</div>
 </section>
 
 <style lang="postcss">
 	section {
 		@apply h-screen w-full relative;
-	}
-	img {
-		@apply absolute h-full w-full object-cover;
-	}
-	span {
-		@apply absolute h-full w-full bg-gray-800 opacity-75;
-	}
-	div {
-		@apply relative h-full flex flex-col justify-center items-center text-neutral-100 gap-10 p-4 text-center;
-	}
-	h1 {
-		@apply font-oswald text-4xl leading-normal;
-	}
-	h3 {
-		@apply text-lg;
+		img {
+			@apply absolute h-full w-full object-cover;
+		}
+		span {
+			@apply absolute h-full w-full bg-gray-800 opacity-75;
+		}
+		div {
+			@apply relative h-full flex flex-col justify-center items-center text-neutral-100 gap-10 p-4 text-center;
+			h1 {
+				@apply font-oswald text-4xl leading-normal;
+			}
+			h3 {
+				@apply text-lg;
+			}
+		}
 	}
 </style>
