234afdc9f2e303c459f1b36b68ef0242c71534fb
maxpiano <max@bilov.de>
2022-08-14 20:51:42 +0200

    finish hero component

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -2,38 +2,55 @@
 	import Button from './Button.svelte';
 
 	import { LL, locale } from '$i18n/i18n-svelte';
+	import ScrollDown from './ScrollDown.svelte';
+	import { browser } from '$app/env';
 
 	export let image = '/images/placeholder.webp';
 	export let heading = 'Title';
 	export let subheading = 'Subtitle';
+
+	/** @type { HTMLElement } */
+	let thisSection;
+
+	const handleScrollDown = () => {
+		const nextSection = thisSection.nextElementSibling;
+		if (nextSection) nextSection.scrollIntoView({ behavior: 'smooth' });
+	};
 </script>
 
-<section>
+<section bind:this={thisSection}>
 	<img src={image} alt="" />
 	<span />
-	<div>
-		<h1>{heading}</h1>
-		<h3>{subheading}</h3>
+	<div class="outer">
+		<div class="inner">
+			<h1>{heading}</h1>
+			<h3>{subheading}</h3>
+		</div>
 		<a href="/{$locale}/contact"><Button>{$LL.contact_button()}</Button></a>
+		<ScrollDown on:click={handleScrollDown} />
 	</div>
 </section>
 
 <style lang="postcss">
 	section {
-		@apply h-screen w-full relative;
+		@apply relative;
 		img {
 			@apply absolute h-full w-full object-cover;
 		}
 		span {
 			@apply absolute h-full w-full bg-gray-800 opacity-75;
 		}
-		div {
-			@apply relative h-full flex flex-col justify-center items-center text-neutral-100 gap-10 p-4 text-center;
-			h1 {
-				@apply font-oswald text-4xl leading-normal;
-			}
-			h3 {
-				@apply text-lg;
+		div.outer {
+			@apply min-h-screen relative h-full flex flex-col justify-evenly items-center
+			text-neutral-100 gap-12 pt-24 p-4 text-center;
+			div.inner {
+				@apply flex flex-col gap-10;
+				h1 {
+					@apply font-oswald text-4xl leading-normal;
+				}
+				h3 {
+					@apply text-lg;
+				}
 			}
 		}
 	}
