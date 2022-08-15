358acf4c000849101ab7efbd31ccffb940d6098a
maxpiano <max@bilov.de>
2022-08-15 13:04:33 +0200

    style index examples

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -2,6 +2,7 @@
 	import Hero from '$lib/Hero.svelte';
 
 	import { LL, locale } from '$i18n/i18n-svelte';
+	import Button from '$lib/Button.svelte';
 
 	const examples =
 		/** @type {Array.<keyof import('$i18n/i18n-types').Translation['index']['examples']>} */
@@ -15,9 +16,23 @@
 />
 
 {#each examples as example}
-	<h2>{$LL.index.examples[example].title()}</h2>
-	<h3>{$LL.index.examples[example].subtitle()}</h3>
-	<a href="/{$locale}/contact"><button>{$LL.contact_button_offer()}</button></a>
-	<p>{$LL.index.examples[example].text()}</p>
+	<div>
+		<h2>{$LL.index.examples[example].title()}</h2>
+		<p>{$LL.index.examples[example].subtitle()}</p>
+		<p>{$LL.index.examples[example].text()}</p>
+		<a href="/{$locale}/contact"><Button dark>{$LL.contact_button_offer()}</Button></a>
+	</div>
 	<img src={$LL.index.examples[example].img()} alt="" />
 {/each}
+
+<style lang="postcss">
+	div {
+		@apply flex flex-col gap-4 p-4 pb-8;
+		h2 {
+			@apply font-oswald text-3xl leading-normal;
+		}
+		a {
+			/* @apply mx-auto; */
+		}
+	}
+</style>
