dee42cc283720bb8257f99fce77bffb97428b6de
maxpiano <max@bilov.de>
2022-08-15 14:21:43 +0200

    style footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -16,27 +16,76 @@
 </script>
 
 <footer>
-	<h2>{$LL.footer.social_media_heading()}</h2>
-	<h3>{$LL.footer.social_media_subheading()}</h3>
+	<div id="footer-heading">
+		<h2>{$LL.footer.social_media_heading()}</h2>
+		<h3>{$LL.footer.social_media_subheading()}</h3>
+	</div>
 
-	{#each social_media as item}
-		<a href={item.link} target="_blank">
-			<Icon name={item.id} />
-		</a>&nbsp;
-	{/each}
+	<div id="social-media">
+		{#each social_media as item}
+			<a href={item.link} target="_blank">
+				<Icon class="fill-gray-800" name={item.id} size={1.5} />
+			</a>
+		{/each}
+	</div>
 
-	<h4>{$LL.footer.links()}</h4>
-	<a href="/">{$LL.index.title()}</a>
-	<a href="/{$locale}/about">{$LL.about.title()}</a>
-	<a href="/{$locale}/contact">{$LL.contact.title()}</a>
-	<a href="/{$locale}/impressum">Impressum</a>
-	<a href="/{$locale}/datenschutz">Datenschutz</a>
-	<a href="/{$locale}/agb">AGB</a>
+	<div id="links">
+		<div>
+			<h4>{$LL.footer.links()}</h4>
+			<a href="/{$locale}">{$LL.index.title()}</a>
+			<a href="/{$locale}/about">{$LL.about.title()}</a>
+			<a href="/{$locale}/contact">{$LL.contact.title()}</a>
+			<a href="/{$locale}/impressum">Impressum</a>
+			<a href="/{$locale}/datenschutz">Datenschutz</a>
+			<a href="/{$locale}/agb">AGB</a>
+		</div>
+		<div id="services">
+			<h4>{$LL.footer.all_services()}</h4>
+			{#each services as service}
+				<a href="/{$locale}/services/{service}">{$LL.services.all[service].title()}</a>
+			{/each}
+		</div>
+	</div>
 
-	<h4>{$LL.footer.all_services()}</h4>
-	{#each services as service}
-		<a href="/{$locale}/services/{service}">{$LL.services.all[service].title()}</a><br />
-	{/each}
-
+	<hr />
 	<p>{$LL.footer.copyright({ year: new Date().getFullYear() })}</p>
 </footer>
+
+<style lang="postcss">
+	footer {
+		@apply bg-gray-800 text-gray-200 px-4 py-6 flex flex-col items-stretch gap-8 mt-4;
+		#footer-heading {
+			@apply flex flex-col gap-2 text-center;
+			h2 {
+				@apply text-2xl font-oswald;
+			}
+		}
+		#social-media {
+			@apply flex gap-4 justify-center flex-wrap;
+			a {
+				@apply p-2 bg-gray-200 rounded-full w-11 aspect-square flex items-center justify-center;
+			}
+		}
+		#links {
+			@apply flex gap-16 justify-between px-4;
+			div {
+				@apply flex flex-col gap-2;
+				h4 {
+					@apply text-sm font-oswald uppercase;
+				}
+				a {
+					@apply text-sm text-gray-400;
+				}
+			}
+			#services {
+				@apply text-right;
+			}
+		}
+		hr {
+			@apply border-gray-500;
+		}
+		p {
+			@apply text-sm text-gray-500 text-center;
+		}
+	}
+</style>
