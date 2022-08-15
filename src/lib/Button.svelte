57bfb295acccb0066d4fe12a46e25cf147e2f10c
maxpiano <max@bilov.de>
2022-08-15 13:04:18 +0200

    add dark style to button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,9 +1,13 @@
-<button>
+<script>
+	export let dark = false;
+</script>
+
+<button class={dark ? 'bg-gray-800 text-neutral-100' : 'bg-neutral-100 text-gray-800'}>
 	<slot />
 </button>
 
 <style lang="postcss">
 	button {
-		@apply bg-neutral-100 text-black font-oswald px-6 py-4;
+		@apply font-oswald px-6 py-4 font-normal;
 	}
 </style>
