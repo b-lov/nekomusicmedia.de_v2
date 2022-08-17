5c3a2d12d9495b4f093793dc5ea142d6fcd0143e
maxpiano <max@bilov.de>
2022-08-17 16:14:57 +0200

    extract contact components

--- src/routes/[lang]/contact.svelte
+++ src/routes/[lang]/contact.svelte
@@ -4,11 +4,31 @@
 	import GoogleMap from '$lib/GoogleMap.svelte';
 </script>
 
-<h2>{$LL.contact.heading()}</h2>
-<p style="white-space: pre-line">{$LL.contact.subheading()}</p>
-<ContactForm />
+<section>
+	<h2>{$LL.contact.heading()}</h2>
+	<p style="white-space: pre-line">{$LL.contact.subheading()}</p>
 
-<h2>{$LL.contact.contact_data.heading()}</h2>
-<p style="white-space: pre-line">{$LL.contact.contact_data.subheading()}</p>
-<GoogleMap />
-<p style="white-space: pre-line">{@html $LL.contact.contact_data.address()}</p>
+	<ContactForm />
+
+	<h2>{$LL.contact.contact_data.heading()}</h2>
+	<p style="white-space: pre-line">{$LL.contact.contact_data.subheading()}</p>
+
+	<GoogleMap />
+
+	<p style="white-space: pre-line">{@html $LL.contact.contact_data.address()}</p>
+</section>
+
+<style lang="postcss">
+	section {
+		@apply flex flex-col items-center gap-8 px-4 py-8;
+		h2 {
+			@apply font-oswald text-4xl leading-normal;
+		}
+		p {
+			@apply prose text-center;
+			&:last-child {
+				@apply text-left;
+			}
+		}
+	}
+</style>
