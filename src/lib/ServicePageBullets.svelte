0cb92e32eb0c3a3e97a30ae3e2f8eef48f3e9903
maxpiano <max@bilov.de>
2022-08-17 12:26:05 +0200

    add service page bullets component

---
+++ src/lib/ServicePageBullets.svelte
@@ -0,0 +1,36 @@
+<script>
+	//
+	export let title = '';
+	/** @type { (() => String)[] } */
+	export let bullets;
+</script>
+
+<article>
+	<h2>{title}</h2>
+	<ul>
+		{#each bullets as bullet}
+			<li>
+				{bullet()}
+			</li>
+		{/each}
+	</ul>
+</article>
+
+<style lang="postcss">
+	article {
+		@apply flex flex-col gap-8 px-4 py-8 bg-gray-800 text-gray-100;
+		h2 {
+			@apply text-4xl font-oswald;
+		}
+		ul {
+			@apply flex flex-col gap-2;
+			li {
+				/* @apply text-lg; */
+				&:before {
+					content: '✓';
+					@apply mr-4 text-3xl;
+				}
+			}
+		}
+	}
+</style>
