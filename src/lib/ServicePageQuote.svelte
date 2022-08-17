8134c15ee8b7c3b32ab9167a963892c335076acb
maxpiano <max@bilov.de>
2022-08-17 13:17:38 +0200

    style service page quote

--- src/lib/ServicePageQuote.svelte
+++ src/lib/ServicePageQuote.svelte
@@ -4,5 +4,17 @@
 	export let quote = '';
 </script>
 
-<Icon name="quote" />
-<h2>{quote}</h2>
+<div>
+	<Icon name="quote" class="absolute fill-gray-300 top-4 left-4 opacity-70" size={10} />
+	<h2>{quote}</h2>
+</div>
+
+<style lang="postcss">
+	div {
+		@apply relative p-12;
+		h2 {
+			@apply relative text-4xl text-gray-500 font-oswald leading-relaxed;
+			hyphens: auto;
+		}
+	}
+</style>
