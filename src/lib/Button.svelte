4c6234bbc9ef228437096dccf72a805301a005f5
maxpiano <max@bilov.de>
2022-08-17 16:14:17 +0200

    pass classes for button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,13 +1,17 @@
 <script>
+	import { compute_rest_props } from 'svelte/internal';
+
 	export let dark = false;
 </script>
 
-<button class={dark ? 'bg-gray-800 text-neutral-100' : 'bg-neutral-100 text-gray-800'}>
+<button
+	class="{$$props.class} {dark ? 'bg-gray-800 text-neutral-100' : 'bg-neutral-100 text-gray-800'}"
+>
 	<slot />
 </button>
 
 <style lang="postcss">
 	button {
-		@apply font-oswald px-6 py-4 font-normal shadow-md;
+		@apply font-oswald px-6 py-4 font-normal shadow-md tracking-wide;
 	}
 </style>
