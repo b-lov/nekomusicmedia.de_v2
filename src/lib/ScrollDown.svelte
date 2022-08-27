bb22c2d57fb058ec75c31f9230b7befc057fc5c6
maxpiano <max@bilov.de>
2022-08-27 17:32:15 +0200

    wanted to make buttons better, but fuck this shit, improved ScrollDown

--- src/lib/ScrollDown.svelte
+++ src/lib/ScrollDown.svelte
@@ -1,27 +1,23 @@
-<button on:click>
-	<span />
-	<span />
-	<span />
+<button on:click class="relative w-12 pt-20 focus:outline-none focus:ring-2">
+	{#each [1, 2, 3] as item}
+		<span
+			class="absolute top-0 border-l border-b border-gray-100 opacity-0 box-border
+			left-1/2 w-6 h-6 -ml-3 -rotate-45 {item === 2 && 'top-4'} {item === 3 && 'top-8'}"
+		/>
+	{/each}
 </button>
 
 <style lang="postcss">
-	button {
-		@apply relative w-12 pt-20;
-	}
 	span {
-		@apply absolute top-0 border-l border-b border-gray-100 opacity-0 box-border left-1/2
-    w-6 h-6 -ml-3 -rotate-45;
 		animation: blink 2s infinite;
 	}
 	span:nth-of-type(1) {
 		animation-delay: 0s;
 	}
 	span:nth-of-type(2) {
-		top: 16px;
 		animation-delay: 0.15s;
 	}
 	span:nth-of-type(3) {
-		top: 32px;
 		animation-delay: 0.3s;
 	}
 	@keyframes blink {
