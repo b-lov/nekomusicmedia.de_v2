27843d02e4865db019d2a9dad4af2591db286362
maxpiano <max@bilov.de>
2022-09-02 23:45:29 +0200

    manually adding catalogue data

---
+++ src/routes/[lang]/catalog/catalog.js
@@ -0,0 +1,47 @@
+export default {
+	speakers: [
+		{ manufacturer: 'QSC', model: 'KLA181', price: 65 },
+		{ manufacturer: 'QSC', model: 'KLA122', price: 65 },
+		{ manufacturer: 'DB Technologies', model: 'Vio S118', price: 70 },
+		{ manufacturer: 'DB Technologies', model: 'Vio S218', price: 90 },
+		{ manufacturer: 'DB Technologies', model: 'Vio L210', price: 70 },
+		{ manufacturer: 'DB Technologies', model: 'Vio L208', price: 65 },
+		{ manufacturer: 'DB Technologies', model: 'Vio X 10', price: 55 },
+		{ manufacturer: 'DB Technologies', model: 'Vio X 12', price: 55 },
+		{ manufacturer: 'DB Technologies', model: 'Vio ES 1203', price: 95 },
+		{ manufacturer: 'DB Technologies', model: 'Flexsys F12', price: 45 },
+		{ manufacturer: 'DB Technologies', model: 'Sub 615', price: 45 },
+		{ manufacturer: 'DB Technologies', model: 'Flexsys FM12', price: 40 },
+		{ manufacturer: 'Turbosound', model: 'IP 2000', price: 75 }
+	]
+};
+
+export const bla = [
+	{
+		sound: [
+			{
+				speakers: [
+					{ manufacturer: 'QSC', model: 'KLA181', price: 65 },
+					{ manufacturer: 'QSC', model: 'KLA122', price: 65 },
+					{ manufacturer: 'DB Technologies', model: 'Vio S118', price: 70 },
+					{ manufacturer: 'DB Technologies', model: 'Vio S218', price: 90 },
+					{ manufacturer: 'DB Technologies', model: 'Vio L210', price: 70 },
+					{ manufacturer: 'DB Technologies', model: 'Vio L208', price: 65 },
+					{ manufacturer: 'DB Technologies', model: 'Vio X 10', price: 55 },
+					{ manufacturer: 'DB Technologies', model: 'Vio X 12', price: 55 },
+					{ manufacturer: 'DB Technologies', model: 'Vio ES 1203', price: 95 },
+					{ manufacturer: 'DB Technologies', model: 'Flexsys F12', price: 45 },
+					{ manufacturer: 'DB Technologies', model: 'Sub 615', price: 45 },
+					{ manufacturer: 'DB Technologies', model: 'Flexsys FM12', price: 40 },
+					{ manufacturer: 'Turbosound', model: 'IP 2000', price: 75 }
+				]
+			}
+		]
+	},
+	{ microphopnes: [{ cabled: [] }, { wireless: [] }] },
+	{ mixers: [{ analogue: [] }, { digital: [] }, { dj: [] }] },
+	{ light: [{ moving: [] }] },
+	{ video: [{ projectors: [] }, { screens: [] }] },
+	{ recording: [{ interfaces: [] }, { monitoring: [] }] },
+	{ michellaneous: [{ electronics: [] }, { stage: [] }, { stands: [] }] }
+];
