ed752c84d0226470fee629fa16c848430a601cf4
maxpiano <max@bilov.de>
2022-09-05 23:22:30 +0200

    this is fucking ugly, but im too lazy to do it better

--- src/routes/[lang]/catalog/index.svelte
+++ src/routes/[lang]/catalog/index.svelte
@@ -1,31 +1,60 @@
 <script>
 	import items from './catalog';
+	import LL from '$i18n/i18n-svelte';
 
 	var formatter = new Intl.NumberFormat('de-DE', {
 		style: 'currency',
 		currency: 'EUR'
 	});
+
+	/** @typedef {keyof import('$i18n/i18n-types').Translation['catalog']['categories']} categoryTranslation */
+	/** @typedef {keyof import('$i18n/i18n-types').Translation['catalog']['categories'][categoryTranslation]['subcategories']} subcategoryTranslation */
+
+	/** @param {Object} category */
+	const categoryHeading = (category) => {
+		const categoryName = /** @type {categoryTranslation} */ (Object.keys(category)[0]);
+		return $LL.catalog.categories[categoryName].title();
+	};
+
+	/**
+	 * @param {Object} category,
+	 * @param {Object} subcategory
+	 */
+	const subcategoryHeading = (category, subcategory) => {
+		const categoryName = /** @type {categoryTranslation} */ (Object.keys(category)[0]);
+		const subcategoryName = Object.keys(subcategory)[0];
+		// @ts-ignore
+		return $LL.catalog.categories[categoryName].subcategories[subcategoryName]();
+	};
 </script>
 
-<main class="prose max-w-4xl mx-auto px-4 py-8">
-	<h1>Sound</h1>
-	<h2>Lautsprecher</h2>
-	<table class="table-auto">
-		<thead>
-			<tr>
-				<th class="">Hersteller</th>
-				<th class="">Modell</th>
-				<th class="">Preis (inkl. MwSt.)</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each items.speakers as item}
-				<tr>
-					<td>{item.manufacturer}</td>
-					<td>{item.model}</td>
-					<td>{formatter.format(item.price)}</td>
-				</tr>
+<main
+	class="prose max-w-4xl mx-auto px-4 py-8 prose-h1:font-oswald prose-h2:font-oswald prose-headings:font-medium"
+>
+	{#each items as category}
+		<section class="pb-4">
+			<h1>{categoryHeading(category)}</h1>
+			{#each Object.values(category)[0] as subcategory}
+				<h2>{subcategoryHeading(category, subcategory)}</h2>
+				<table class="table-auto">
+					<thead>
+						<tr>
+							<th>{$LL.catalog.table.manufacturer()}</th>
+							<th>{$LL.catalog.table.model()}</th>
+							<th>{$LL.catalog.table.price()}</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each Object.values(subcategory)[0] as product}
+							<tr>
+								<td>{product.manufacturer}</td>
+								<td>{product.model}</td>
+								<td>{formatter.format(product.price)}</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
 			{/each}
-		</tbody>
-	</table>
+		</section>
+	{/each}
 </main>
